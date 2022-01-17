801941cf5446b297442a455ed6ae22d3018552bb
Whitney Krape <whitney@formfunction.io>
2022-01-17 15:57:19 -0500

    Starting to clean up after changing formatting.

--- block-article/build/index.asset.php
+++ block-article/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '94ce0c8ecf90f373fa3b69f76d3a7e6f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'a259c053cb97566e1686b0f8db54570c');
