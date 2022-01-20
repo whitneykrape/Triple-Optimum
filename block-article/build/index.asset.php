d1c087362139231a39af32f7aa255663eb82d64a
Whitney Krape <whitney@formfunction.io>
2022-01-20 07:54:21 -0500

    Refining.

--- block-article/build/index.asset.php
+++ block-article/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '1e3778f220fb06abbbdc758a1c79f3aa');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'b863bd2b9388b4aa3f919c74de5a2434');
--- block-article/src/index.js
+++ block-article/src/index.js
@@ -209,30 +209,32 @@ registerBlockType( 'block-article/article-block', {
 					target="_blank"
 					rel="noopener noreferrer"
 				>
+					{ mediaURL && <img src={ mediaURL } alt="" /> }
+				</a>
+				<div className="article-text-block">
+					<RichText.Content
+						tagName="h3"
+						className="article-title"
+						value={ title }
+					/>
 					<RichText.Content
 						tagName="span"
 						className="image-marker image-marker-date"
 						value={ date }
 					/>
-					{ mediaURL && <img src={ mediaURL } alt="" /> }
-				</a>
-				<RichText.Content
-					tagName="h3"
-					className="article-title"
-					value={ title }
-				/>
-				<RichText.Content
-					tagName="p"
-					className="article-description"
-					value={ description }
-				/>
-				<RichText.Content tagName="p" className="article-read-more">
 					<RichText.Content
-						tagName="span"
-						className="btn-sm article-link"
-						value={ link }
+						tagName="p"
+						className="article-description"
+						value={ description }
 					/>
-				</RichText.Content>
+					<RichText.Content tagName="p" className="article-read-more">
+						<RichText.Content
+							tagName="span"
+							className="btn-sm article-link"
+							value={ link }
+						/>
+					</RichText.Content>
+				</div>
 			</article>
 		);
 	},
